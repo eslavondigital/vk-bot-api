0a763bc1b023498b0af7266c904ed8a5f6985381
Victor <eslavondigital@gmail.com>
2020-04-11 12:20:18 +0300

    Update search_docs_bot.php

--- example/search_docs_bot.php
+++ example/search_docs_bot.php
@@ -4,12 +4,10 @@ require_once __DIR__ . '../vendor/autoload.php';
 use GuzzleHttp\Client;
 use Eslavon\VkBotApi\VkBotApi;
 
-
 echo "ok";
 
 $vk = new VkBotApi(new Client(), '6b09ff759c99dd5c41d5fe017d70a1749a575178d7b4962ce66ec6a1b2b0c682e8bd6951ef3db3f3dfd19');
-$json = file_get_contents(__DIR__ . '/test/event/messages/message_new.json');
-$event = $vk->callback()->run($json);
+$event = $vk->callback()->run();
 
 switch ($vk->callback()->getEventType()) {
     case "message_new";
